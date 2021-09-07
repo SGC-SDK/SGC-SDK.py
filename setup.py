6affd5fa2521d56ec059b9fb17675c4013a040a4
Your Name <iimasatoshi@Tki-MacBook-Pro.local>
2021-09-07 20:03:21 +0900

    setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='SGC-SDK.py',
+    version="0.0.1",
+    description="Super Global Chat Library for Discord.py",
+    long_description="",
+    author='Tsukikoh',
+    url="https://github.com/SGC-SDK/SGC-SDK.py",
+    license='Apache License 2.0',
+    classifiers=[
+        "Development Status :: 1 - Planning"
+    ]
+)
