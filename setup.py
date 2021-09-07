02b7a4ea0ead958b4d711c3e02dc50161ffdb129
Tsukikoh <35320102+Tsukikoh@users.noreply.github.com>
2021-09-07 20:25:53 +0900

    add install_requires

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name='SGC-SDK.py',
     version="0.0.1",
+    install_requires=["discord.py @ git+https://github.com/Rapptz/discord.py@rewrite#egg=discord.py[voice]"],
     description="Super Global Chat Library for Discord.py",
     long_description="",
     author='Tsukikoh',
